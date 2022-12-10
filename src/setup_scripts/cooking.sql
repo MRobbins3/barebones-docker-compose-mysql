1522d3132d9d2ef9842d796a9be68256fadd156b
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2022-12-10 02:18:05 -0600

    Update cooking.sql
    add ingredients.

--- src/setup_scripts/cooking.sql
+++ src/setup_scripts/cooking.sql
@@ -19,6 +19,7 @@ CREATE TABLE recipes (
 
 INSERT INTO recipes VALUES 
 ('Dinner Rolls', 'Esther Ketcham', 15, 15, 135),
+('Dinner Rolls', 'Patricia Baker', 18, 20, 90),
 ('Cinnamon Rolls', 'Patricia Baker', 18, 30, 75), 
 ('Pumpkin Bars', 'Carly Duytschaver', 30, 22, 20), 
 ('Chicken Noodle Soup', 'Esther Ketcham', 8, 180, 30),
@@ -33,6 +34,26 @@ CREATE TABLE ingredients (
   PRIMARY KEY (ingredient_ID)
 );
 
+INSERT INTO ingredients VALUES 
+('flour', 'baking', 'pantry'),
+('butter', 'dairy', 'fridge'),
+('yeast', 'baking', 'fridge'), 
+('sugar', 'baking', 'pantry'), 
+('salt', 'spice', 'cabinet'), 
+('milk', 'dairy', 'fridge'), 
+('cinnamon', 'spice', 'cabinet'), 
+('CRISCO', 'baking', 'pantry'),
+('powdered sugar', 'baking', 'pantry'),
+('cream cheese', 'dairy', 'fridge'),
+('pumpkin puree', 'canned', 'pantry'), 
+('chicken thighs', 'poultry', 'freezer'), 
+('egg noodles', 'dry goods', 'pantry'), 
+('chicken stock', 'canned', 'pantry'), 
+('mixed veggies', 'vegetables', 'freezer'), 
+('chicken gravy mix', 'dry goods', 'pantry'), 
+('minute rice', 'dry goods', 'pantry')
+;
+
 CREATE TABLE recipe_ingredients (
   recipe_name varchar(50) NOT NULL,
   recipe_creator varchar(30) NOT NULL,
@@ -44,5 +65,8 @@ CREATE TABLE recipe_ingredients (
   FOREIGN KEY (ingredient_id) REFERENCES igredients (ingredient_id)
 );
 
+INSERT INTO recipe_ingredients VALUES
+()
+;
 
 
